9f9568f618b8f1f16dcabd38d97054a9dcffc50c
vasylevskyi <vasilevskiy@gmail.com>
2024-03-14 00:44:43 +0200

    Added Tasks 1-3

---
+++ src/test/java/ua/goit/javadevmodule14springbootcore/SpringBootCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.goit.javadevmodule14springbootcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
